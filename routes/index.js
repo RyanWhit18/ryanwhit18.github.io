f952723445f34ef71786133ecb629743a1a003c3
RAWhitf18 <ryanwhitfield27@gmail.com>
2020-08-20 22:17:32 -0400

    Add more routes and set up pug files for other pages

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,36 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.get('/home', (req, res) => {
+    res.render('index');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+router.get('/music', (req, res) => {
+    res.render('music');
+});
+
+router.get('/videos', (req, res) => {
+    res.render('videos');
+});
+
+router.get('/shop', (req, res) => {
+    res.render('shop');
+});
+
+router.get('/gallery', (req, res) => {
+    res.render('gallery');
+});
+
+router.get('/tour', (req, res) => {
+    res.render('tour');
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
 module.exports = router;
