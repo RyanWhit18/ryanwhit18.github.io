2c99c5547f9c99bde9ed4eae586a20d2ff04dccf
RAWhitf18 <ryanwhitfield27@gmail.com>
2020-08-22 23:11:34 -0400

    Add server back to the project and move static files to public

---
+++ app.js
@@ -0,0 +1,11 @@
+/**** Import Libraries ****/
+const express = require('express');
+const port = process.env.PORT || 8080
+
+/**** Set Up ****/
+const app = express();
+app.use(express.static('public'));
+
+const server = app.listen(port, () => {
+    console.log("Server started at port: " + port);
+})
