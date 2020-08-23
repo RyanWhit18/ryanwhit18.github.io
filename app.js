fc90a1da81d2700550cafd9c390ea7fbe228bf8d
RAWhitf18 <ryanwhitfield27@gmail.com>
2020-08-22 23:38:49 -0400

    Remove html extensions in nav

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080
 
 /**** Set Up ****/
 const app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public', {extensions: ['html', 'htm']}));
 
 const server = app.listen(port, () => {
     console.log("Server started at port: " + port);
