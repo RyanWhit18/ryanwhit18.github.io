534cdafd35403908b8bb830bbee16618fcdcfd46
Ryan Whitfield <ryanwhitfield27@gmail.com>
2020-08-25 11:45:16 -0400

    Make header and footer javascript files for easy editing (#15)

---
+++ header.js
@@ -0,0 +1,44 @@
+document.write('<header>'+
+'<div class="header-area">'+
+'<div id="header" class="main-header-area">'+
+'<div class="container-fluid">'+
+'<div class="header_bottom_border">'+
+'<div class="row align-items-center">'+
+'<div class="col-xl-3 col-lg-2"></div>'+
+'<div class="col-xl-6 col-lg-7">'+
+'<div class="main-menu d-none d-lg-block">'+
+'<nav>'+
+'<ul id="navigation">'+
+'<li><a class="active" href="index">Home</a></li>'+
+'<li><a href="about">About</a></li>'+
+'<li><a href="track">Tracks</a></li>'+
+'<li><a href="video">Videos</a></li>'+
+'<li><a href="contact">Contact</a></li>'+
+'</ul>'+
+'</nav>'+
+'</div>'+
+'</div>'+
+'<div class="col-xl-3 col-lg-3 d-none d-lg-block">'+
+'<div class="social_icon text-right">'+
+'<ul>'+
+'<li>'+
+'<a href="#"> <i class="fa fa-facebook"></i> </a>'+
+'</li>'+
+'<li>'+
+'<a href="#"> <i class="fa fa-twitter"></i> </a>'+
+'</li>'+
+'<li>'+
+'<a href="#"> <i class="fa fa-instagram"></i> </a>'+
+'</li>'+
+'</ul>'+
+'</div>'+
+'</div>'+
+'<div class="col-12">'+
+'<div class="mobile_menu d-block d-lg-none"></div>'+
+'</div>'+
+'</div>'+
+'</div>'+
+'</div>'+
+'</div>'+
+'</div>'+
+'</header>');
